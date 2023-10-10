1f2f5d0ab5fbf32e2a130a206e812536067df496
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-10 11:14:28 +0900

    :memo: JwtProperties 작성

---
+++ src/main/kotlin/andreas311/miso/global/security/jwt/properties/JwtProperties.kt
@@ -0,0 +1,22 @@
+package andreas311.miso.global.security.jwt.properties
+
+import io.jsonwebtoken.security.Keys
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import java.security.Key
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+class JwtProperties(
+    accessSecret: String,
+    refreshSecret: String
+) {
+
+    val accessSecret: Key
+    val refreshSecret: Key
+
+    init {
+        this.accessSecret = Keys.hmacShaKeyFor(accessSecret.toByteArray())
+        this.refreshSecret = Keys.hmacShaKeyFor(refreshSecret.toByteArray())
+    }
+}
