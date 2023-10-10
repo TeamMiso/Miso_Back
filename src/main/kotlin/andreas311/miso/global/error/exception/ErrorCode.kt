472f09596b596f9fb1a88231b08e109634fbe946
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-10 16:11:09 +0900

    :memo: ErrorCode 에 회원가입, mail 관련 에러 enum 추가

--- src/main/kotlin/andreas311/miso/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/andreas311/miso/global/error/exception/ErrorCode.kt
@@ -8,9 +8,14 @@ enum class ErrorCode(
     // SERVER ERROR
     UNKNOWN_ERROR(500, "알 수 없는 에러입니다."),
 
+    // MAIL
+    EMAIL_SEND_FAIL(500, "사용자를 찾을 수 없습니다."),
+
     // USER
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
     ROLE_NOT_EXIST(404, "역할이 존재하지 않습니다"),
+    USER_ALREADY_EXIST(409, "이미 사용자가 존재합니다."),
+    MISMATCH_PASSWORD(400, "비밀번호가 일치하지 않습니다."),
 
     // TOKEN
     TOKEN_IS_EXPIRED(401, "토큰이 만료 되었습니다."),
