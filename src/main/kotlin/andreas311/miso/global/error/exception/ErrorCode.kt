f728517d6d4ca6ed2f88643d39072f85c17b27ff
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-10 11:15:29 +0900

    :memo: ErrorCode 에 User 관련 예외 enum 추가

--- src/main/kotlin/andreas311/miso/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/andreas311/miso/global/error/exception/ErrorCode.kt
@@ -8,6 +8,10 @@ enum class ErrorCode(
     // SERVER ERROR
     UNKNOWN_ERROR(500, "알 수 없는 에러입니다."),
 
+    // USER
+    USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
+    ROLE_NOT_EXIST(404, "역할이 존재하지 않습니다"),
+
     // TOKEN
     TOKEN_IS_EXPIRED(401, "토큰이 만료 되었습니다."),
     TOKEN_NOT_VALID(401, "토큰이 유효 하지 않습니다."),
