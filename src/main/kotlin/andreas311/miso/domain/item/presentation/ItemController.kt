1f869aa4010cdaf3069ac6543e5206d8e925d8da
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-12 12:22:48 +0900

    :sparkles: ItemController 에 아이템 리스트 api 등록

--- src/main/kotlin/andreas311/miso/domain/item/presentation/ItemController.kt
+++ src/main/kotlin/andreas311/miso/domain/item/presentation/ItemController.kt
@@ -1,7 +1,9 @@
 package andreas311.miso.domain.item.presentation
 
 import andreas311.miso.domain.item.presentation.data.response.DetailItemResponseDto
+import andreas311.miso.domain.item.presentation.data.response.ListItemResponseDto
 import andreas311.miso.domain.item.service.DetailItemService
+import andreas311.miso.domain.item.service.ListItemService
 import andreas311.miso.global.annotation.RequestController
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -10,8 +12,14 @@ import org.springframework.web.bind.annotation.PathVariable
 
 @RequestController("/item")
 class ItemController(
+    private val listItemService: ListItemService,
     private val detailItemService: DetailItemService
 ) {
+
+    @GetMapping
+    fun list(): ResponseEntity<ListItemResponseDto> =
+        listItemService.execute()
+            .let { ResponseEntity.status(HttpStatus.OK).body(it) }
 
     @GetMapping("/{id}")
     fun detail(@PathVariable id: Long): ResponseEntity<DetailItemResponseDto> =
