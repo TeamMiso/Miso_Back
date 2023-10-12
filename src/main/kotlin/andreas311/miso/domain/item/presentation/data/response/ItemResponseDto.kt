d9b6e44d9d28b1c2957d8358b03c2b01a45aa4d6
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-12 16:05:34 +0900

    :memo: ItemResponseDto 에 constructor 추가

--- src/main/kotlin/andreas311/miso/domain/item/presentation/data/response/ItemResponseDto.kt
+++ src/main/kotlin/andreas311/miso/domain/item/presentation/data/response/ItemResponseDto.kt
@@ -1,6 +1,7 @@
 package andreas311.miso.domain.item.presentation.data.response
 
 import andreas311.miso.domain.item.entity.Item
+import andreas311.miso.domain.purchase.entity.Purchase
 
 data class ItemResponseDto(
     val price: Int,
@@ -11,5 +12,11 @@ data class ItemResponseDto(
         price = item.price,
         name = item.name,
         imageUrl = item.imageUrl
+    )
+
+    constructor(purchase: Purchase) : this(
+        price = purchase.item.price,
+        name = purchase.item.name,
+        imageUrl = purchase.item.imageUrl
     )
 }
