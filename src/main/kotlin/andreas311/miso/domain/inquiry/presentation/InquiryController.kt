d469887e8787302676770745b64b9de2cdf6a827
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-13 14:13:48 +0900

    :sparkles: InquiryController 에 내가 작성한 문의글 리스트 api 등록

--- src/main/kotlin/andreas311/miso/domain/inquiry/presentation/InquiryController.kt
+++ src/main/kotlin/andreas311/miso/domain/inquiry/presentation/InquiryController.kt
@@ -1,17 +1,21 @@
 package andreas311.miso.domain.inquiry.presentation
 
 import andreas311.miso.domain.inquiry.presentation.data.request.WriteInquiryRequestDto
+import andreas311.miso.domain.inquiry.presentation.data.response.ListInquiryResponseDto
+import andreas311.miso.domain.inquiry.service.ListMyInquiryService
 import andreas311.miso.domain.inquiry.service.WriteInquiryService
 import andreas311.miso.global.annotation.RequestController
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.multipart.MultipartFile
 
 @RequestController("/inquiry")
 class InquiryController(
-    private val writeInquiryService: WriteInquiryService
+    private val writeInquiryService: WriteInquiryService,
+    private val listMyInquiryService: ListMyInquiryService
 ) {
 
     @PostMapping
@@ -21,5 +25,10 @@ class InquiryController(
     ): ResponseEntity<Void> =
         writeInquiryService.execute(writeInquiryRequestDto, multipartFile)
             .let { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @GetMapping
+    fun inquiryLog(): ResponseEntity<ListInquiryResponseDto> =
+        listMyInquiryService.execute()
+            .let { ResponseEntity.status(HttpStatus.OK).body(it) }
 
 }
