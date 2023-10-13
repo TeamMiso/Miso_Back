bd9eaacc8e5da0f3f0d7ce63432b23883c0f0abc
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-13 14:12:06 +0900

    :memo: GetPurchaseItemService 에서 ListMyPurchaseItemService 으로 이름 수정 및 오류 수정

--- src/main/kotlin/andreas311/miso/domain/purchase/service/impl/ListMyPurchaseItemServiceImpl.kt
+++ src/main/kotlin/andreas311/miso/domain/purchase/service/impl/ListMyPurchaseItemServiceImpl.kt
@@ -2,19 +2,17 @@ package andreas311.miso.domain.purchase.service.impl
 
 import andreas311.miso.domain.item.presentation.data.response.ItemResponseDto
 import andreas311.miso.domain.item.presentation.data.response.ListItemResponseDto
-import andreas311.miso.domain.item.repository.ItemRepository
 import andreas311.miso.domain.purchase.exception.PurchaseNotFoundException
 import andreas311.miso.domain.purchase.repository.PurchaseRepository
-import andreas311.miso.domain.purchase.service.GetPurchaseItemService
+import andreas311.miso.domain.purchase.service.ListMyPurchaseItemService
 import andreas311.miso.global.annotation.ReadOnlyService
 import andreas311.miso.global.util.UserUtil
 
 @ReadOnlyService
-class GetPurchaseItemServiceImpl(
+class ListMyPurchaseItemServiceImpl(
     private val userUtil: UserUtil,
-    private val itemRepository: ItemRepository,
     private val purchaseRepository: PurchaseRepository
-) : GetPurchaseItemService {
+) : ListMyPurchaseItemService {
 
     override fun execute(): ListItemResponseDto {
 
@@ -24,8 +22,7 @@ class GetPurchaseItemServiceImpl(
             ?: throw PurchaseNotFoundException()
 
         return ListItemResponseDto(
-            itemList = itemRepository.findAllByOrderByPriceAsc()
-                .map { ItemResponseDto(purchase) }
+            itemList = purchase.map { ItemResponseDto(it) }
         )
     }
 }
