a10d21ba87c203efa75740ebbd9be36b1b389bf6
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-17 12:09:20 +0900

    :sparkles: InquiryController 에 문의글 상세보기 api 등록

--- src/main/kotlin/andreas311/miso/domain/inquiry/presentation/InquiryController.kt
+++ src/main/kotlin/andreas311/miso/domain/inquiry/presentation/InquiryController.kt
@@ -1,13 +1,16 @@
 package andreas311.miso.domain.inquiry.presentation
 
 import andreas311.miso.domain.inquiry.presentation.data.request.WriteInquiryRequestDto
+import andreas311.miso.domain.inquiry.presentation.data.response.DetailInquiryResponseDto
 import andreas311.miso.domain.inquiry.presentation.data.response.ListInquiryResponseDto
+import andreas311.miso.domain.inquiry.service.DetailInquiryService
 import andreas311.miso.domain.inquiry.service.ListMyInquiryService
 import andreas311.miso.domain.inquiry.service.WriteInquiryService
 import andreas311.miso.global.annotation.RequestController
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.multipart.MultipartFile
@@ -15,7 +18,8 @@ import org.springframework.web.multipart.MultipartFile
 @RequestController("/inquiry")
 class InquiryController(
     private val writeInquiryService: WriteInquiryService,
-    private val listMyInquiryService: ListMyInquiryService
+    private val listMyInquiryService: ListMyInquiryService,
+    private val detailInquiryService: DetailInquiryService,
 ) {
 
     @PostMapping
@@ -29,6 +33,11 @@ class InquiryController(
     @GetMapping
     fun inquiryLog(): ResponseEntity<ListInquiryResponseDto> =
         listMyInquiryService.execute()
+            .let { ResponseEntity.status(HttpStatus.OK).body(it) }
+
+    @GetMapping("/{id}")
+    fun detail(@PathVariable id: Long): ResponseEntity<DetailInquiryResponseDto> =
+        detailInquiryService.execute(id)
             .let { ResponseEntity.status(HttpStatus.OK).body(it) }
 
 }
