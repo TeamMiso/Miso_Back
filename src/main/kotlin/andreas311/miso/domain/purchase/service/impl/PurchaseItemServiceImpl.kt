c4042013b6f9d8e66d6271c6e858c6c6eb6f09a0
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-30 13:39:38 +0900

    :recycle: PurchaseItemServiceImpl 에 toEntity 메서드 분리

--- src/main/kotlin/andreas311/miso/domain/purchase/service/impl/PurchaseItemServiceImpl.kt
+++ src/main/kotlin/andreas311/miso/domain/purchase/service/impl/PurchaseItemServiceImpl.kt
@@ -1,11 +1,13 @@
 package andreas311.miso.domain.purchase.service.impl
 
+import andreas311.miso.domain.item.entity.Item
 import andreas311.miso.domain.item.exception.ItemNotFoundException
 import andreas311.miso.domain.item.repository.ItemRepository
 import andreas311.miso.domain.purchase.entity.Purchase
 import andreas311.miso.domain.purchase.exception.PointNotEnoughException
 import andreas311.miso.domain.purchase.repository.PurchaseRepository
 import andreas311.miso.domain.purchase.service.PurchaseItemService
+import andreas311.miso.domain.user.entity.User
 import andreas311.miso.global.annotation.RollbackService
 import andreas311.miso.global.util.UserUtil
 import org.springframework.data.repository.findByIdOrNull
@@ -30,6 +32,13 @@ class PurchaseItemServiceImpl(
 
         user.removePoint(item.price)
 
-        purchaseRepository.save(Purchase(0L, user, item))
+        purchaseRepository.save(toEntity(user, item))
     }
+
+    private fun toEntity(user: User, item: Item): Purchase =
+        Purchase(
+            id = 0L,
+            user = user,
+            item = item
+        )
 }
