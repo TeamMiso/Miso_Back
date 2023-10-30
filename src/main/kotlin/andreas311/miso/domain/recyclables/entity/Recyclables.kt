fd1cc31c6b5a9671ce23ae0f6dfb7d7d59025fac
uuuuuuuk <s22034@gsm.hs.kr>
2023-10-30 15:12:11 +0900

    :memo: Recyclables 엔티티 설계

---
+++ src/main/kotlin/andreas311/miso/domain/recyclables/entity/Recyclables.kt
@@ -0,0 +1,34 @@
+package andreas311.miso.domain.recyclables.entity
+
+import andreas311.miso.domain.recyclables.enums.RecyclablesType
+import org.hibernate.annotations.DynamicUpdate
+import javax.persistence.*
+
+@Entity
+@DynamicUpdate
+class Recyclables(
+
+    @Id
+    @Column(name = "recyclables_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(name = "title")
+    val title: String,
+
+    @Column(name = "sub_title")
+    val subTitle: String,
+
+    @Column(name = "content")
+    val content: String,
+
+    @Column(name = "imageUrl")
+    val imageUrl: String,
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "recyclables_type")
+    val recyclablesType: RecyclablesType,
+
+    @Column(name = "recycle_mark")
+    val recycleMark: String
+)
