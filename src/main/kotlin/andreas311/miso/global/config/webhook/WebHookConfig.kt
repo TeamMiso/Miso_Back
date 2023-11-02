0a9239a1cfc206f321b2076dcf7c58d22e1aa50c
uuuuuuuk <s22034@gsm.hs.kr>
2023-11-02 14:17:21 +0900

    :memo: WebHookConfig 작성

---
+++ src/main/kotlin/andreas311/miso/global/config/webhook/WebHookConfig.kt
@@ -0,0 +1,14 @@
+package andreas311.miso.global.config.webhook
+
+import okhttp3.OkHttpClient
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class WebHookConfig {
+
+    private val httpClient = OkHttpClient()
+
+    @Bean
+    fun httpClient() = httpClient
+}
