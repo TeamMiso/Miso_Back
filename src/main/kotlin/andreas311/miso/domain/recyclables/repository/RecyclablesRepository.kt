006ca1892075b78de67808e273f7b3d83cdf9268
uuuuuuuk <s22034@gsm.hs.kr>
2023-11-02 16:22:48 +0900

    :memo: RecyclablesRepository 가 RecyclablesRepositoryCustom 를 구현하게 추가

--- src/main/kotlin/andreas311/miso/domain/recyclables/repository/RecyclablesRepository.kt
+++ src/main/kotlin/andreas311/miso/domain/recyclables/repository/RecyclablesRepository.kt
@@ -4,7 +4,7 @@ import andreas311.miso.domain.recyclables.entity.Recyclables
 import andreas311.miso.domain.recyclables.enums.RecyclablesType
 import org.springframework.data.repository.CrudRepository
 
-interface RecyclablesRepository : CrudRepository<Recyclables, Long> {
+interface RecyclablesRepository : CrudRepository<Recyclables, Long>, RecyclablesRepositoryCustom {
 
     fun findByRecyclablesType(recyclablesType: RecyclablesType): Recyclables?
 }
