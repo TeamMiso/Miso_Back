d6ace9c3225d104d5158bc678a4a6293cf5d7fa5
uuuuuuuk <s22034@gsm.hs.kr>
2023-11-02 16:20:55 +0900

    :recycle: 회원가입 로직 수정

--- src/main/kotlin/andreas311/miso/domain/auth/service/impl/SignUpServiceImpl.kt
+++ src/main/kotlin/andreas311/miso/domain/auth/service/impl/SignUpServiceImpl.kt
@@ -4,6 +4,7 @@ import andreas311.miso.domain.auth.exception.MismatchPasswordException
 import andreas311.miso.domain.auth.exception.UserAlreadyExistException
 import andreas311.miso.domain.auth.presentation.data.request.SignUpRequestDto
 import andreas311.miso.domain.auth.service.SignUpService
+import andreas311.miso.domain.email.repository.EmailRepository
 import andreas311.miso.domain.email.service.EmailSendService
 import andreas311.miso.domain.user.repository.UserRepository
 import andreas311.miso.global.annotation.RollbackService
@@ -12,21 +13,26 @@ import org.springframework.security.crypto.password.PasswordEncoder
 @RollbackService
 class SignUpServiceImpl(
     private val userRepository: UserRepository,
+    private val emailRepository: EmailRepository,
     private val emailSendService: EmailSendService,
     private val passwordEncoder: PasswordEncoder
 ) : SignUpService {
 
     override fun execute(signUpRequestDto: SignUpRequestDto) {
 
-        if (userRepository.existsByEmail(signUpRequestDto.email)) {
+        emailSendService.execute(signUpRequestDto.email)
+
+        val email = emailRepository.findByEmail(signUpRequestDto.email)
+
+        if (userRepository.existsByEmail(signUpRequestDto.email) && email.authentication) {
             throw UserAlreadyExistException()
+        } else if(userRepository.existsByEmail(signUpRequestDto.email)) {
+            userRepository.deleteByEmail(signUpRequestDto.email)
         }
 
         if (signUpRequestDto.password != signUpRequestDto.passwordCheck) {
             throw MismatchPasswordException()
         }
-
-        emailSendService.execute(signUpRequestDto.email)
 
         val encodedPassword = passwordEncoder.encode(signUpRequestDto.password)
 
