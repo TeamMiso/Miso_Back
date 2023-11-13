6dc53f2127ae2d83667881830e52321d36fcaf93
uuuuuuuk <s22034@gsm.hs.kr>
2023-11-13 11:42:59 +0900

    :recycle: MisoApplication 에 EnableScheduling 어노테이션 제거

--- src/main/kotlin/andreas311/miso/MisoApplication.kt
+++ src/main/kotlin/andreas311/miso/MisoApplication.kt
@@ -3,10 +3,8 @@ package andreas311.miso
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableScheduling
 import java.util.*
 
-@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class MisoApplication
