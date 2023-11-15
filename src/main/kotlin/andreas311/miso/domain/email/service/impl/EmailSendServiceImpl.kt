728ff95eac7305e031cdaadbdb22148a7046f877
uuuuuuuk <s22034@gsm.hs.kr>
2023-11-15 12:08:33 +0900

    :ambulance: 이메일 회원가입 관련 로직 수정

--- src/main/kotlin/andreas311/miso/domain/email/service/impl/EmailSendServiceImpl.kt
+++ src/main/kotlin/andreas311/miso/domain/email/service/impl/EmailSendServiceImpl.kt
@@ -1,21 +1,20 @@
 package andreas311.miso.domain.email.service.impl
 
-import andreas311.miso.domain.email.repository.EmailRepository
 import andreas311.miso.domain.email.service.EmailSendService
+import andreas311.miso.domain.user.repository.UserRepository
 import andreas311.miso.global.annotation.RollbackService
 import andreas311.miso.global.email.EmailSender
 
 @RollbackService
 class EmailSendServiceImpl(
     private val emailSender: EmailSender,
-    private val emailRepository: EmailRepository
+    private val userRepository: UserRepository
 ) : EmailSendService {
+
     override fun execute(email: String) {
 
-        if (emailRepository.existsByEmail(email)) {
-            emailRepository.deleteByEmail(email)
+        if (!userRepository.existsByEmail(email)) {
+            emailSender.execute(email)
         }
-
-        emailSender.execute(email)
     }
 }
