f297aea897ebee1c2e0b30052270c869e7eddead
uuuuuuuk <s22034@gsm.hs.kr>
2023-11-27 23:26:07 +0900

    :memo: 테스트

---
+++ src/main/kotlin/andreas311/miso/domain/imageprocess/service/ImageProcessService.kt
@@ -0,0 +1,11 @@
+package andreas311.miso.domain.imageprocess.service
+
+import org.springframework.web.multipart.MultipartFile
+
+class ImageProcessService {
+
+    fun execute(multipartFile: MultipartFile) {
+
+        ()
+    }
+}
