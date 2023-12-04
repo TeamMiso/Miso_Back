b089a647c25e894ef1541286a4b869590f8ddaf3
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-04 14:15:20 +0900

    :sparkles: RecyclablesController 에 재활용품 전체 리스트 api 등록

--- src/main/kotlin/andreas311/miso/domain/recyclables/presentation/RecyclablesController.kt
+++ src/main/kotlin/andreas311/miso/domain/recyclables/presentation/RecyclablesController.kt
@@ -2,8 +2,10 @@ package andreas311.miso.domain.recyclables.presentation
 
 import andreas311.miso.domain.recyclables.enums.RecyclablesType
 import andreas311.miso.domain.recyclables.presentation.data.response.DetailRecyclablesResponseDto
+import andreas311.miso.domain.recyclables.presentation.data.response.ListSearchRecyclablesResponseDto
 import andreas311.miso.domain.recyclables.presentation.data.response.SearchRecyclablesResponseDto
 import andreas311.miso.domain.recyclables.service.DetailRecyclablesService
+import andreas311.miso.domain.recyclables.service.ListAllRecyclablesService
 import andreas311.miso.domain.recyclables.service.SearchRecyclablesService
 import andreas311.miso.global.annotation.RequestController
 import org.springframework.http.HttpStatus
@@ -14,7 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam
 @RequestController("/recyclables")
 class RecyclablesController(
     private val detailRecyclablesService: DetailRecyclablesService,
-    private val searchRecyclablesService: SearchRecyclablesService
+    private val searchRecyclablesService: SearchRecyclablesService,
+    private val listAllRecyclablesService: ListAllRecyclablesService
 ) {
 
     @GetMapping
@@ -25,5 +28,10 @@ class RecyclablesController(
     @GetMapping("/search")
     fun search(@RequestParam searchValue: String): ResponseEntity<SearchRecyclablesResponseDto> =
         searchRecyclablesService.execute(searchValue)
+            .let { ResponseEntity.status(HttpStatus.OK).body(it) }
+
+    @GetMapping("/all")
+    fun recyclablesAll(): ResponseEntity<ListSearchRecyclablesResponseDto> =
+        listAllRecyclablesService.execute()
             .let { ResponseEntity.status(HttpStatus.OK).body(it) }
 }
