312e740a0700c9903a7d3126da8964c2dd3eaf79
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-05 08:32:20 +0900

    :memo: ProcessRecyclablesImageService 의 반환값을 DetailRecyclablesResponseDto 에서 ListDetailRecyclablesResponseDto 로 변경

--- src/main/kotlin/andreas311/miso/domain/recyclables/service/ProcessRecyclablesImageService.kt
+++ src/main/kotlin/andreas311/miso/domain/recyclables/service/ProcessRecyclablesImageService.kt
@@ -1,9 +1,9 @@
 package andreas311.miso.domain.recyclables.service
 
-import andreas311.miso.domain.recyclables.presentation.data.response.DetailRecyclablesResponseDto
+import andreas311.miso.domain.recyclables.presentation.data.response.ListDetailRecyclablesResponseDto
 import org.springframework.web.multipart.MultipartFile
 
 interface ProcessRecyclablesImageService {
 
-    fun execute(multipartFile: MultipartFile): DetailRecyclablesResponseDto
+    fun execute(multipartFile: MultipartFile): ListDetailRecyclablesResponseDto
 }
