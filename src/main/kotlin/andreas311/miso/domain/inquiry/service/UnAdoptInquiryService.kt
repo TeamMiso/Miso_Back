f601693c1aa97a95e73269ddf28c626d55eabde1
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-12 14:01:00 +0900

    :sparkles: UnAdoptInquiryService 에 푸시 알림 전송 기능 및 notification save 로직 추가

--- src/main/kotlin/andreas311/miso/domain/inquiry/service/UnAdoptInquiryService.kt
+++ src/main/kotlin/andreas311/miso/domain/inquiry/service/UnAdoptInquiryService.kt
@@ -1,6 +1,9 @@
 package andreas311.miso.domain.inquiry.service
 
+import andreas311.miso.domain.notification.entity.Notification
+import andreas311.miso.domain.notification.presentation.data.request.WriteNotificationRequestDto
+
 interface UnAdoptInquiryService {
 
-    fun execute(id: Long)
+    fun execute(id: Long, writeNotificationRequestDto: WriteNotificationRequestDto): Notification
 }
