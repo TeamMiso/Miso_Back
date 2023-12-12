5051ede58d131e40bac85914adec6628c220f082
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-12 13:58:32 +0900

    :memo: NotificationRepository 작성

---
+++ src/main/kotlin/andreas311/miso/domain/notification/repository/NotificationRepository.kt
@@ -0,0 +1,7 @@
+package andreas311.miso.domain.notification.repository
+
+import andreas311.miso.domain.notification.entity.Notification
+import org.springframework.data.repository.CrudRepository
+
+interface NotificationRepository : CrudRepository<Notification, Long> {
+}
