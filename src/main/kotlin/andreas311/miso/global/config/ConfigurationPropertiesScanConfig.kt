9e8a7e25f747a3e508f7b40a14e439ada8ef0ee6
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-12 14:07:08 +0900

    :memo: ConfigurationPropertiesScanConfig 에 FCM 관련 basePackages 추가

--- src/main/kotlin/andreas311/miso/global/config/ConfigurationPropertiesScanConfig.kt
+++ src/main/kotlin/andreas311/miso/global/config/ConfigurationPropertiesScanConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConfigurationPropertiesScan(basePackages =
-["andreas311.miso.global.redis.properties",
-"andreas311.miso.global.security.jwt.properties"])
+["andreas311.miso.global.config.redis.properties",
+"andreas311.miso.global.security.jwt.properties",
+"andreas311.miso.global.config.notification.properties"])
 class ConfigurationPropertiesScanConfig
