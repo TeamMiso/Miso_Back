1e229a783727f39c5cf0203e254633ab04adb05d
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-15 09:47:49 +0900

    :memo: NotificationRepository 에 findByInquiryId 메서드 추가

--- src/main/kotlin/andreas311/miso/domain/notification/repository/NotificationRepository.kt
+++ src/main/kotlin/andreas311/miso/domain/notification/repository/NotificationRepository.kt
@@ -4,4 +4,6 @@ import andreas311.miso.domain.notification.entity.Notification
 import org.springframework.data.repository.CrudRepository
 
 interface NotificationRepository : CrudRepository<Notification, Long> {
+
+    fun findByInquiryId(id: Long): Notification?
 }
