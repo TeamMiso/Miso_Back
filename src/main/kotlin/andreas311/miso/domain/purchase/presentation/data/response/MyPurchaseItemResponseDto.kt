62dbce693955fba0d9d0e0f22af304b1a3456fcc
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-16 01:09:18 +0900

    :memo: 내 구매 내역 리스트의 반환값에 imageUrl 필드 추가

--- src/main/kotlin/andreas311/miso/domain/purchase/presentation/data/response/MyPurchaseItemResponseDto.kt
+++ src/main/kotlin/andreas311/miso/domain/purchase/presentation/data/response/MyPurchaseItemResponseDto.kt
@@ -7,12 +7,14 @@ data class MyPurchaseItemResponseDto(
     val id: Long,
     val price: Int,
     val name: String,
+    val imageUrl: String,
     val createdDate: LocalDateTime
 ) {
     constructor(purchase: Purchase) : this(
         id = purchase.item.id,
         price = purchase.item.price,
         name = purchase.item.name,
+        imageUrl = purchase.item.imageUrl,
         createdDate = purchase.createdDate
     )
 }
