2e6a9c6edeea747f226db923e136b8935914ed3b
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-15 10:11:31 +0900

    :fire: UnAdoptInquiryService 삭제

--- src/main/kotlin/andreas311/miso/domain/inquiry/service/UnAdoptInquiryService.kt
+++
@@ -1,9 +0,0 @@
-package andreas311.miso.domain.inquiry.service
-
-import andreas311.miso.domain.notification.entity.Notification
-import andreas311.miso.domain.notification.presentation.data.request.WriteNotificationRequestDto
-
-interface UnAdoptInquiryService {
-
-    fun execute(id: Long, writeNotificationRequestDto: WriteNotificationRequestDto): Notification
-}
