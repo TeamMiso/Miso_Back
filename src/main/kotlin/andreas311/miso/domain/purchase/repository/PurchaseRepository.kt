4059f035a282bd4bc3203e8433f5415cccbf3877
uuuuuuuk <s22034@gsm.hs.kr>
2023-12-20 17:05:35 +0900

    :memo: PurchaseRepository 의 find 메서드에 내림차순 옵션 추가

--- src/main/kotlin/andreas311/miso/domain/purchase/repository/PurchaseRepository.kt
+++ src/main/kotlin/andreas311/miso/domain/purchase/repository/PurchaseRepository.kt
@@ -6,5 +6,5 @@ import org.springframework.data.repository.CrudRepository
 
 interface PurchaseRepository : CrudRepository<Purchase, Long> {
 
-    fun findAllByUser(user: User): List<Purchase>?
+    fun findAllByUserOrderByCreatedDateDesc(user: User): List<Purchase>?
 }
