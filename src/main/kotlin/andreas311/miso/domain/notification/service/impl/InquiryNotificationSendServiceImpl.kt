c0cc049287f800fc7f16306c70ab4ef979c200a3
uuuuuuuk <s22034@gsm.hs.kr>
2024-04-18 10:23:50 +0900

    :memo: InquiryNotificationSendServiceImpl 에 content 필드의 이름을 body 로 변경

--- src/main/kotlin/andreas311/miso/domain/notification/service/impl/InquiryNotificationSendServiceImpl.kt
+++ src/main/kotlin/andreas311/miso/domain/notification/service/impl/InquiryNotificationSendServiceImpl.kt
@@ -15,7 +15,7 @@ class InquiryNotificationSendServiceImpl(
 
     override fun execute(inquiry: Inquiry, token: String) {
         val notificationType = when (inquiry.inquiryStatus) {
-            InquiryStatus.WAIT -> NotificationType.INQUIRT_WAIT
+            InquiryStatus.WAIT -> NotificationType.INQUIRY_WAIT
             InquiryStatus.COMPLETE -> NotificationType.INQUIRY_COMPLETE
         }
 
@@ -24,7 +24,7 @@ class InquiryNotificationSendServiceImpl(
                 deviceToken = token,
                 notificationAlarm = NotificationAlarm(
                     title = notificationType.title,
-                    content = notificationType.content,
+                    body = notificationType.body,
                     writer = "미소"
                 )
             )
